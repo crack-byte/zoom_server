ba77a01f06110e84c3914b59d2daa7889fa96c36
abhishek <abhishek.purohit@oodlestechnologies.com>
2021-03-28 17:58:33 +0530

    update ci/cd

--- src/main/java/com/crackbyte/controller/ShowsController.java
+++ src/main/java/com/crackbyte/controller/ShowsController.java
@@ -3,23 +3,25 @@ package com.crackbyte.controller;
 import com.crackbyte.dto.ShowsDTO;
 import com.crackbyte.services.TvShowsService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/shows")
 public class ShowsController {
-    RestTemplate restTemplate;
+    private static final Logger log = LoggerFactory.getLogger(ShowsController.class);
     TvShowsService tvShowsService;
 
     @GetMapping
     public ResponseEntity<Object> getShows(@RequestParam(defaultValue = "") String q, @RequestParam(defaultValue = "1") int page) {
+        log.info("searching {}......", q);
         ShowsDTO data;
         if (!q.isEmpty()) {
             data = tvShowsService.searchShows(q, page);
